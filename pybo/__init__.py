82ee320ad90fae0a586f288894b3963e4ae4aede
Suin Choi <suin6301@gmail.com>
2024-01-06 23:26:34 +0900

    answering function is added

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -17,9 +17,9 @@ def create_app():
     from . import models
 
     # register the blueprint from views/main_views.py
-    from .views import main_views, question_views
+    from .views import main_views, question_views, answer_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
-
+    app.register_blueprint(answer_views.bp)
 
     return app
